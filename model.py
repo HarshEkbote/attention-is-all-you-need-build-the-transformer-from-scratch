caf85f755354b0183d33d95049b5fc76ab774543
Harsh Ekbote <101455597+HarshEkbote@users.noreply.github.com>
2026-08-01 10:57:26 +0530

    Step 56: shift_targets_right_with_start_token

--- model.py
+++ model.py
@@ -644,8 +644,13 @@ def collect_model_parameters_into_list(encoder_layer_params, decoder_layer_param
 
     return params
 
-# Step 56 - shift_targets_right_with_start_token (not yet solved)
-# TODO: implement
+# Step 56 - shift_targets_right_with_start_token
+def shift_targets_right_with_start_token(target_ids, start_token_id):
+    # TODO: prepend start_token_id and drop the last column so output shape matches target_ids
+    shifted=torch.empty_like(target_ids)
+    shifted[:,0]=start_token_id
+    shifted[:,1:]=target_ids[:,:-1]
+    return shifted
 
 # Step 57 - compute_noam_learning_rate (not yet solved)
 # TODO: implement
